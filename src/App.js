d775dc55e3102f79e3f11a362e363f0781c3c251
akshayack <akshayamaheshan@gmail.com>
2019-04-06 09:32:02 +0530

    create

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+  constructor(){
+    super();
+    this.state={data:""
+  }
+  this.displayMessage=this.displayMessage.bind(this)
+  };
+
+  displayMessage(){
+    var today=new Date()
+    var curHr=today.getHours()
+    if(curHr<12){
+      this.setState({data:"Good morning Akshaya"})
+    }
+    else if(curHr<18){
+      this.setState({data:"Good afternoon Akshaya"})
+    }
+    else{
+      this.setState({data:"Good evening Akshaya"})
+    }
+  } 
+  
+  render(){
+    return(
+      <div>
+        <button onClick={this.displayMessage}>click</button>
+        <hi>{this.state.data}</hi>
       </div>
     );
-  }
-}
 
-export default App;
+    }
+  }
+  export default App;
